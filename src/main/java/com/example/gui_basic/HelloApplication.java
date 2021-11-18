f38923aa323d5897a964c7e1e63f415a51131657
Albert <jonas.albert.oktatas@gmail.com>
2021-11-18 18:06:53 +0100

    Restructure code - remove eventhandler superclass

--- src/main/java/com/example/gui_basic/HelloApplication.java
+++ src/main/java/com/example/gui_basic/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.example.gui_basic;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 
@@ -16,7 +17,7 @@ import javafx.geometry.HPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class HelloApplication extends Application implements EventHandler {
+public class HelloApplication extends Application {
 
     Button button;
 
@@ -24,45 +25,44 @@ public class HelloApplication extends Application implements EventHandler {
         launch(args);
     }
 
+    GridPane rootNode = new GridPane();
+    TextField Value1 = new TextField();
+    TextField Value2 = new TextField();
+    Button aButton = new Button("Label");
+    TextField result = new TextField();
+    Scene scene = new Scene(rootNode, 300, 250);
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Title");
-
-        GridPane rootNode = new GridPane();
         rootNode.setPadding(new Insets(15));
         rootNode.setHgap(5);
         rootNode.setVgap(5);
         rootNode.setAlignment(Pos.CENTER);
+        GridPane.setHalignment(aButton, HPos.LEFT);
+        result.setEditable(false);
+        primaryStage.setScene(scene);
 
         rootNode.add(new Label("Value1"), 0, 0);
-        TextField Value1 = new TextField();
         rootNode.add(Value1, 1, 0);
         rootNode.add(new Label("Value2"), 0, 1);
-        TextField Value2 = new TextField();
         rootNode.add(Value2, 1, 1);
         rootNode.add(new Label("Action"), 0, 2);
-        Button aButton = new Button("Label");
         rootNode.add(aButton, 1, 2);
-        GridPane.setHalignment(aButton, HPos.LEFT);
-        TextField result = new TextField();
-        result.setEditable(false);
         rootNode.add(result, 1, 3);
 
-        Scene scene = new Scene(rootNode, 300, 250);
-        primaryStage.setScene(scene);
         primaryStage.show();
 
-        aButton.setOnAction(e -> {
-            Integer value1 = Integer.valueOf(Value1.getText());
-            Integer value2 = Integer.valueOf(Value2.getText());
-            Integer r = value1 + value2;
-            result.setText(r.toString());
-        });
-    }
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Integer value1 = Integer.valueOf(Value1.getText());
+                Integer value2 = Integer.valueOf(Value2.getText());
+                Integer r = value1 + value2;
+                result.setText(r.toString());
+            }
+        };
 
-    @Override
-    public void handle(Event event) {
-        if(event.getSource() == button){
-        }
+        aButton.setOnAction(event);
     }
 }
