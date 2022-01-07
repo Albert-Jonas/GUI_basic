4be886bd157bef4649d8157d38b7410818f67c24
Bozso69 <bozsoky.zoltan@bozsofalvi.hu>
2022-01-07 18:30:50 +0100

    V0.42. T0.13 - IF Sorozat

---
+++ src/main/java/com/example/gui_basic/ifSorozat.java
@@ -0,0 +1,61 @@
+package com.example.gui_basic;
+
+import java.util.Scanner;
+
+
+public class ifSorozat {
+
+
+    public static void main(String[] args) {
+        ifSorozat_Util iutil = new ifSorozat_Util();
+
+        String bekerSzoveg, text;
+        int szam;
+
+        bekerSzoveg = bekeres();
+        System.out.println("bekerSzoveg = " + bekerSzoveg);
+        System.out.println("A bekért szöveg : " + bekerSzoveg);
+        text = textSzetvalaszt(bekerSzoveg);
+        szam = szamSzetvalszt(bekerSzoveg);
+        System.out.println("text = " + text);
+        System.out.println("szam = " + szam);
+        iutil.szam(szam);
+    }
+
+    private static int szamSzetvalszt(String bekerSzoveg) {
+        StringBuilder karakterek = new StringBuilder();
+        char[] k = {'0','1','2','3','4','5','6','7','8','9'};
+        for (int i = 0; i < bekerSzoveg.length(); i++) {
+            Boolean isB = false;
+            for (int j = 0; j < k.length; j++) {
+                if (bekerSzoveg.charAt(i) == k[j]){
+                    isB = true;
+                }
+            }
+            karakterek.append(isB ? bekerSzoveg.charAt(i) : "");
+        }
+        return Integer.parseInt(karakterek.toString());}
+
+
+
+    private static String textSzetvalaszt(String bekerSzoveg) {
+        StringBuilder karakterek = new StringBuilder();
+        char[] k = {'0','1','2','3','4','5','6','7','8','9'};
+        for (int i = 0; i < bekerSzoveg.length(); i++) {
+            Boolean isB = false;
+            for (int j = 0; j < k.length; j++) {
+                if (bekerSzoveg.charAt(i) == k[j]){
+                    isB = true;
+            }
+            }
+            karakterek.append(!isB ? bekerSzoveg.charAt(i) : "");
+        }
+    return karakterek.toString();}
+
+    private static String bekeres() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kérek egy karaktarlácot(Csaz 0-9 aA-zZ) : ");
+        return sc.nextLine();
+    }
+
+}
---
+++ src/main/java/com/example/gui_basic/ifSorozat_Util.java
@@ -0,0 +1,38 @@
+package com.example.gui_basic;
+
+public class ifSorozat_Util {
+
+//1
+    public void szam(int szam) {
+        if (szam > 0 ){
+            System.out.println("A szám pozitív szam");
+        }
+//3
+        if (szam == 0 ){
+            System.out.println("A szám nulla");
+        }
+//4
+        if (szam == 0 ){
+            System.out.println("A szám nulla");
+        }
+
+
+
+    }
+}
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//1
+//10
+//11
+//12
+//13
+//14
+//15
