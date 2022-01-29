f907521bd89ae2db1199f30d168453d8650212c7
Bozso69 <bozsoky.zoltan@bozsofalvi.hu>
2022-01-29 23:44:24 +0100

    Static test - logikai visszatérési érték tesztelés

--- src/main/java/com/example/gui_basic/eddigiTesztek.java
+++ src/main/java/com/example/gui_basic/eddigiTesztek.java
@@ -3,10 +3,20 @@ package com.example.gui_basic;
 public class eddigiTesztek {
     public static void main(String[] args) {
         int szam;
-        szam = osszeadas(10,10);
+        boolean isvissza;
+        szam = osszeadas(10, 10);
+        isvissza = nagyobbE(szam, 100);
     }
 
-     static int osszeadas(int i, int i1) {
+    static boolean nagyobbE(int szam, int i) {
+        if (szam > i) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static int osszeadas(int i, int i1) {
         return i + i1;
     }
 
--- src/test/java/com/example/gui_basic/eddigiTesztekTest.java
+++ src/test/java/com/example/gui_basic/eddigiTesztekTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class eddigiTesztekTest {
@@ -22,13 +24,23 @@ class eddigiTesztekTest {
 
     @Test
     void main() {
+        ByteArrayInputStream in = new ByteArrayInputStream("osszeadas".getBytes());
+        System.setIn(in);
+        eddigiTesztek.main(new String[] {"osszeadas"});
+
     }
 
     @Test
-    @DisplayName("Public test - visszatérési érték tesztelés")
+    @DisplayName("Static test - visszatérési érték tesztelés")
     void osszeadas() {
         assertEquals(20, eddigiTesztek.osszeadas(10,10));
         assertNotEquals(40, eddigiTesztek.osszeadas(10,10));
+    }
+    @Test
+    @DisplayName("Static test - logikai visszatérési érték tesztelés")
+    void nagyobbE(){
+        assertFalse(edTesztek.nagyobbE(10,10));
+        assertTrue(edTesztek.nagyobbE(200,10));
 
     }
 }
